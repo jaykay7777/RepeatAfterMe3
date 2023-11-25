dbc55fd4f35401205e09f5f5bf645098a6c788da
jaykay7777 <115580403+jaykay7777@users.noreply.github.com>
2023-11-25 15:34:01 +0000

    added move script
    added script to move object. Tried to tidy the visual layout up so things dont get lost on screen

--- Assets/Scripts/MoveCharacter.cs
+++ Assets/Scripts/MoveCharacter.cs
@@ -2,17 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveCharacter : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+namespace leveldesign{
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public class MoveCharacter : MonoBehaviour
+        {
+        public Transform pointATransform;
+
+        public Transform pointBTransform;
+
+        public float Speed = 2.0f;
+
+        private Vector3 pointA;
+
+        private Vector3 pointB;
+
+        private float journeyLength;
+
+        private float startTime;
+        // Start is called before the first frame update
+        void Start()
+        {
+            if(pointATransform && pointBTransform)
+            {
+                pointA = pointATransform.position;
+                pointB = pointBTransform.position;
+
+                startTime = Time.time;
+                journeyLength = Vector3.Distance(pointA, pointB);
+            }
+            else
+            {
+                Debug.LogWarning("Point Transforms are not set! There will be no movement");
+            }
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+if(pointATransform&& pointBTransform)
+            {
+                float distCovered = (Time.time - startTime) * Speed;
+                float fractionOfJourney = distCovered / journeyLength;
+                transform.position = Vector3.Lerp(pointA, pointB, Mathf.PingPong(fractionOfJourney, 1));
+            }
+        }
+       
     }
 }
